ed504a0fc96a768909cc3a647ba4b2cce70d102d
Winzmcman <nikithobbi@gmail.com>
2020-09-06 13:27:55 +0300

    chat_api2.0(fixed1)

--- api/users/index.php
+++ api/users/index.php
@@ -1,13 +1,12 @@
-<!-- 
-
+<?php
+/*
 Website created with php. Includes API for chatting (client: https://github.com/winzmcman/fartuh-chat)
 Copyright (C) 2020 Nikita Pavlov
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License along with this program. If not, see http://www.gnu.org/licenses/.
-Author's email: nikitafartuh@ukr.net -->
-
-<?php
+Author's email: nikitafartuh@ukr.net
+*/
 
 require_once('controller.php');
 require_once('db.php');
