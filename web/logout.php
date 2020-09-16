4fe7cf6b5543f5b97e550d446ca53f98e5f81780
Winzmcman <nikithobbi@gmail.com>
2020-09-16 14:05:05 +0300

    3.7

--- web/logout.php
+++ web/logout.php
@@ -1,4 +1,12 @@
-<?php
+<?php 
+/*
+Website created with php. Includes API for chatting (client: https://github.com/winzmcman/fartuh-chat)
+Copyright (C) 2020 Nikita Pavlov
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+Author's email: nikitafartuh@ukr.net
+*/
 
 
 setcookie('login', '', time()-10);
